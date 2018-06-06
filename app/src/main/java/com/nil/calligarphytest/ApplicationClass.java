596083515428528342729f09cc412d900028b0d6
nileshatyonomy <richyrichnil$1000B>
2018-06-06 11:50:14 +0530

    test

---
+++ app/src/main/java/com/nil/calligarphytest/ApplicationClass.java
@@ -0,0 +1,22 @@
+package com.nil.calligarphytest;
+
+import android.app.Application;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * Created by Nilesh Thakkar on Dec 05,2016 14:58.
+ */
+public class ApplicationClass extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/orange.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+}
---
+++ app/src/main/java/com/nil/calligarphytest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.nil.calligarphytest;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+}
